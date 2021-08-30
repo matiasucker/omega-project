4138e2e2b3a63222fc9a8531c587a0f402a0b1fe
matiasucker <matias.ucker@gmail.com>
2021-08-30 09:37:46 -0300

    backend created

---
+++ backend/src/test/java/com/matiasucker/omega/OmegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasucker.omega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
