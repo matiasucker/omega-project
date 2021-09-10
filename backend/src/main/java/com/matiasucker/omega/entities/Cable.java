4a27d69b6228d516f030b2f3ccf24d51d7efb6de
matiasucker <matias.ucker@gmail.com>
2021-09-10 12:29:27 -0300

    entity Cable

---
+++ backend/src/main/java/com/matiasucker/omega/entities/Cable.java
@@ -0,0 +1,138 @@
+package com.matiasucker.omega.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_cable")
+public class Cable implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer number;
+	private String code;
+	private String description;
+	private Integer amountFiber;
+	private Integer amountGroup;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+	public Cable() {
+	}
+
+	public Cable(Long id, Integer number, String code, String description, Integer amountFiber, Integer amountGroup) {
+		this.id = id;
+		this.number = number;
+		this.code = code;
+		this.description = description;
+		this.amountFiber = amountFiber;
+		this.amountGroup = amountGroup;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getAmountFiber() {
+		return amountFiber;
+	}
+
+	public void setAmountFiber(Integer amountFiber) {
+		this.amountFiber = amountFiber;
+	}
+
+	public Integer getAmountGroup() {
+		return amountGroup;
+	}
+
+	public void setAmountGroup(Integer amountGroup) {
+		this.amountGroup = amountGroup;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void PreUpdate() {
+		updatedAt = Instant.now();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cable other = (Cable) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
