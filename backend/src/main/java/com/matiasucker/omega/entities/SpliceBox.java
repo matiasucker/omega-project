4d866af3d9eb6d8001c5bf6ef35964d2d717913d
matiasucker <matias.ucker@gmail.com>
2021-09-10 11:22:53 -0300

    entity SpliceBox

---
+++ backend/src/main/java/com/matiasucker/omega/entities/SpliceBox.java
@@ -0,0 +1,83 @@
+package com.matiasucker.omega.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_splice_box")
+public class SpliceBox implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String amountfiber;
+	private String description;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+	public SpliceBox() {
+	}
+
+	public SpliceBox(Long id, String amountfiber, String description) {
+		this.id = id;
+		this.amountfiber = amountfiber;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAmountfiber() {
+		return amountfiber;
+	}
+
+	public void setAmountfiber(String amountfiber) {
+		this.amountfiber = amountfiber;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void PreUpdate() {
+		updatedAt = Instant.now();
+	}
+
+}
